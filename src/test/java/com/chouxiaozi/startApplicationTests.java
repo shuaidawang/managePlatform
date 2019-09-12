c93ebdfc0a7af1ef435d121bbe610ece7558386c
shuaidawang <1065970342@qq.com>
2019-09-12 17:30:44 +0800

    first commit

---
+++ src/test/java/com/chouxiaozi/startApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chouxiaozi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class startApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
