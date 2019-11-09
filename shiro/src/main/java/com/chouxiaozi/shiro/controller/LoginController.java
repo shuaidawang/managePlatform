0c84f932075f37d8d745a34872f72f6619f5feb3
wangshuai <wangshuai@wangkahome.com>
2019-11-09 17:52:08 +0800

    描述：shiro登录页面 提交人：帅

--- shiro/src/main/java/com/chouxiaozi/shiro/controller/LoginController.java
+++ shiro/src/main/java/com/chouxiaozi/shiro/controller/LoginController.java
@@ -4,6 +4,7 @@ import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
@@ -38,5 +39,17 @@ public class LoginController {
         map.put("msg", msg);
         // 此方法不处理登录成功,由shiro进行处理
         return "login";
+    }
+
+    @RequestMapping("/index")
+    public String index(HttpServletRequest request, Map<String, Object> map){
+        System.out.println("跳转到index页面");
+        return "redirect:/index";
+    }
+
+    @RequestMapping("/403")
+    public ModelAndView error403(){
+        System.out.println("跳转到403页面");
+        return new ModelAndView("403");
     }
 }
